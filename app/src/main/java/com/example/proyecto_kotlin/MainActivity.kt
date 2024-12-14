0d504289df545ce5b0382cde4cefe84169467d88
Javier Placencia <javix.plax@gmail.com>
2024-12-14 16:20:19 -0300

    Menú de Vacunas y Antiparasitarios

--- app/src/main/java/com/example/proyecto_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_kotlin/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_ficha, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_ficha, R.id.nav_vacunas
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/proyecto_kotlin/ui/vacunas/VacunasViewModel.kt
+++ app/src/main/java/com/example/proyecto_kotlin/ui/vacunas/VacunasViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.proyecto_kotlin.ui.slideshow
+package com.example.proyecto_kotlin.ui.vacunas
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class VacunasViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Este es el historial de vacunas y antiparasitarios"
     }
     val text: LiveData<String> = _text
 }
