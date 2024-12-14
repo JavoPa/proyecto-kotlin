1116a8fdb5723ae5eaa97d4d41e04bfad13f1553
Javier Placencia <javix.plax@gmail.com>
2024-12-14 16:11:50 -0300

    Menú de Inicio y Ficha Médica base

--- app/src/main/java/com/example/proyecto_kotlin/ui/ficha/FichaFragment.kt
+++ app/src/main/java/com/example/proyecto_kotlin/ui/ficha/FichaFragment.kt
@@ -1,4 +1,4 @@
-package com.example.proyecto_kotlin.ui.gallery
+package com.example.proyecto_kotlin.ui.ficha
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.proyecto_kotlin.databinding.FragmentGalleryBinding
+import com.example.proyecto_kotlin.databinding.FragmentFichaBinding
 
-class GalleryFragment : Fragment() {
+class FichaFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentFichaBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(FichaViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentFichaBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.textFicha
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/proyecto_kotlin/ui/ficha/FichaViewModel.kt
+++ app/src/main/java/com/example/proyecto_kotlin/ui/ficha/FichaViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.proyecto_kotlin.ui.gallery
+package com.example.proyecto_kotlin.ui.ficha
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class FichaViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Esta es la Ficha Medica"
     }
     val text: LiveData<String> = _text
 }
